3ff5bacbfaebda52067de2ae54fbcfcd0cb57363
Lasdepic <clementjordan34@gmail.com>
2025-09-11 14:54:31 +0200

    FormLogin

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -7,7 +7,17 @@ function MyConnexion(){
             <img src={StellarHubWhite} alt="Logo du site" width="150" height="150" />
         </section>
         <section>
-            
+            <form>
+                <div>
+                    <p>Sign in</p>
+                    <label htmlFor="email"></label>
+                    <input type="email" id="email" name="email" placeholder='Email' required />
+                </div>
+                <div>
+                    <label htmlFor="password"></label>
+                    <input type="password" id="password" name="password" placeholder='Password' required />
+                </div>
+            </form>
         </section>
         </main>
     )
