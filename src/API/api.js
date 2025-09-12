67f55f8347b69fc128f26e77cf941c4638c187b8
Lasdepic <clementjordan34@gmail.com>
2025-09-12 15:02:17 +0200

    pas de connexion correction

--- src/API/api.js
+++ src/API/api.js
@@ -24,17 +24,16 @@ async function fetchCommentsByPostId(id) {
 }
 
 async function login(userName, passeWord) {
-  fetch("https://dummyjson.com/auth/login", {
+  const response = await fetch("https://dummyjson.com/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       username: userName,
       password: passeWord,
     }),
-    credentials: "include",
-  })
-    .then((res) => res.json())
-    .then(console.log);
+  });
+  const data = await response.json();
+  return data;
 }
 // async function searchUser(value){
 // fetch(`https://dummyjson.com/users/search?q=${value}`)
@@ -42,4 +41,4 @@ async function login(userName, passeWord) {
 // .then(console.log);
 // }
 
-export { fetchUsers, fetchPost, login };
+export { fetchUsers, fetchPost, login, fetchPostsByUserId };
